16e24bc0670a450afc24d334f0406ad988a79ea9
Jitin Chawla <jchawla@sapient.com>
2017-07-12 21:07:52 +0530

    test

--- main.groovy
+++ main.groovy
@@ -1,5 +1,5 @@
-import models.*
-import templates.*
+//import models.*
+//import templates.*
 
 import hudson.FilePath
 import org.yaml.snakeyaml.Yaml
@@ -19,5 +19,64 @@ void createJobs() {
         def project = projectConfig.project.replaceAll(' ', '-')
 
         PullRequestTemplate.create(job("${project}-Pull-Request"), projectConfig)
+    }
+}
+
+
+class ProjectConfig {
+    /*
+     * Required
+     */
+    String project
+    String repo
+    String email
+
+    /*
+     * Optional
+     */
+    String command_test = "mvn clean test"
+}
+
+class PullRequestTemplate {
+    static void create(job, config) {
+        job.with {
+            description("Builds all pull requests opened against <code>${config.repo}</code>.<br><br><b>Note</b>: This job is managed <a href='https://github.com/curalate/jenkins-job-dsl-demo'>programmatically</a>; any changes will be lost.")
+
+            logRotator {
+                daysToKeep(7)
+                numToKeep(50)
+            }
+
+            concurrentBuild(true)
+
+            scm {
+                git {
+                    remote {
+                        github(config.repo)
+                        refspec('+refs/pull/*:refs/remotes/origin/pr/*')
+                     }
+                     branch('${sha1}')
+                 }
+            }
+
+            triggers {
+                githubPullRequest {
+                    cron('H/5 * * * *')
+                    triggerPhrase('@curalatebot rebuild')
+                    onlyTriggerPhrase(false)
+                    useGitHubHooks(true)
+                    permitAll(true)
+                    autoCloseFailedPullRequests(false)
+                }
+            }
+
+            publishers {
+                githubCommitNotifier()
+            }
+
+            steps {
+                shell(config.command_test)
+            }
+        }
     }
 }
